b49e0804dcb4e757d2dfa164b8c287785d86f310
Amyx000 <armankazi111@gmail.com>
2022-09-19 00:02:00 +0530

    files added

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Content() {
+    return (
+        <>
+            <div className="div1">Hello, I'm Arman</div>
+            <div className="div2">A Full Stack Developer</div>
+            <div className="div3">
+                I used MERN stack technology to create my projects. I am more facinated
+                by CSS, I use vanilla CSS more than any frameworks, because I love to
+                create my own CSS. I use React for all of my frontend part.
+            </div>
+        </>
+    );
+}
+
+export default Content;
